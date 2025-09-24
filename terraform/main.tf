23f35272845e130311f69346c5a84cf196b500d0
anasmirn <anasmirn@cisco.com>
2025-09-24 17:47:09 +0200

    Change ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,23 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
