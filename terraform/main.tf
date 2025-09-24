0430fdb2583ca06a30f53c0d1394a8971d5210b8
anasmirn <anasmirn@cisco.com>
2025-09-24 17:51:28 +0200

    Add network settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,6 +52,40 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
+resource "aws_vpc" "jenkins_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = { Name = "jenkins-vpc" }
+}
+
+resource "aws_subnet" "jenkins_subnet" {
+  vpc_id                  = aws_vpc.jenkins_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = { Name = "jenkins-subnet" }
+}
+
+resource "aws_internet_gateway" "jenkins_igw" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+}
+
+resource "aws_route_table" "jenkins_rt" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.jenkins_igw.id
+  }
+}
+
+resource "aws_route_table_association" "jenkins_rta" {
+  subnet_id      = aws_subnet.jenkins_subnet.id
+  route_table_id = aws_route_table.jenkins_rt.id
+}
+
+
 output "public_ip" {
   value = aws_instance.jenkins.public_ip
 }
