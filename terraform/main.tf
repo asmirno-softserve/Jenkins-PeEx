d4e33f517a41e93725daacfaba6f576557c08193
anasmirn <anasmirn@cisco.com>
2025-09-28 20:48:00 +0200

    change name

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,7 +141,7 @@ resource "aws_iam_role_policy_attachment" "jenkins_ecr_policy" {
 }
 
 # Instance profile for EC2 to use the role
-resource "aws_iam_instance_profile" "jenkins_profile" {
+resource "aws_iam_instance_profile" "jenkins_inst_profile" {
   name = "jenkins-ec2-profile"
   role = aws_iam_role.jenkins_role.name
 }
